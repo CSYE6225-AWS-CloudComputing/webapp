e909457a6786e802fb8ed9577c002a0ab75c326f
Naga Venkata Nishanth Sayana <sayana.n@northeastern.edu>
2024-02-07 19:42:01 -0500

    Digital ocean script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # webapp
 
-Setup JDK 17 or higher on your local
-install maven using the commands mvn -clean and -install
-run the spring boot application using
+## Setup JDK 17 or higher on your local
+## install maven using the commands mvn -clean and -install
+## run the spring boot application using spring-boot:run
---
+++ startup-scripts/install-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update system packages
+sudo yum update -y
+
+# Install unzip and openjdk
+sudo yum install -y java-17-openjdk maven
+
+
+# Install PostgreSQL
+sudo yum install -y postgresql-server postgresql-contrib
+
+# Initialize the PostgreSQL database and start the service
+sudo postgresql-setup initdb
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+
+# Create a PostgreSQL user and database for your Spring Boot project
+sudo -u postgres psql -c "CREATE USER postgres WITH PASSWORD 'root';"
+sudo -u postgres psql -c "CREATE DATABASE webapp WITH OWNER = postgres;"
+
+# Grant necessary privileges to the user
+sudo -u postgres psql -c "ALTER USER postgres WITH SUPERUSER;"
