b26e74f0f09cc7080e34908a9c947b35e536981b
Naga Venkata Nishanth Sayana <sayana.n@northeastern.edu>
2024-02-08 19:00:58 -0500

    vm script updated

--- startup-scripts/install-db.sh
+++ startup-scripts/install-db.sh
@@ -6,6 +6,9 @@ sudo yum update -y
 # Install unzip and openjdk
 sudo yum install -y java-17-openjdk maven
 
+#Java version change
+echo 2 | sudo alternatives --config java
+
 
 # Install PostgreSQL
 sudo yum install -y postgresql-server postgresql-contrib
@@ -16,8 +19,11 @@ sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
 # Create a PostgreSQL user and database for your Spring Boot project
-sudo -u postgres psql -c "CREATE USER postgres WITH PASSWORD 'root';"
+sudo -u postgres psql -c "ALTER USER postgres with password 'root';"
 sudo -u postgres psql -c "CREATE DATABASE webapp WITH OWNER = postgres;"
 
 # Grant necessary privileges to the user
 sudo -u postgres psql -c "ALTER USER postgres WITH SUPERUSER;"
+
+
+
