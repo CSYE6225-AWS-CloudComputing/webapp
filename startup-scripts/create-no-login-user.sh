2e4247a998cbfef55b7b6330bc54fb7a59d92c4f
Naga Venkata Nishanth Sayana <sayana.n@northeastern.edu>
2024-02-20 23:10:56 -0500

    modifies startup scripts to unzip proper files

--- startup-scripts/create-no-login-user.sh
+++ startup-scripts/create-no-login-user.sh
@@ -4,9 +4,10 @@ sudo groupadd csye6225
 #add user to group
 sudo useradd -s /usr/sbin/nologin -g csye6225 csye6225
 
-sudo unzip /home/csye6225/webapp/webapp.zip
 echo "========================================================================="
-ls /home/csye6225/webapp
+pwd
+echo "========================================================================="
+ls /home/csye6225/webapp/
 echo "========================================================================="
 sudo cp /home/csye6225/webapp/startup-scripts/webapp.service /etc/systemd/system
 
