97561c2b84ecbeb48a9ccfe41b41cf3f9f1758ea
Naga Venkata Nishanth Sayana <sayana.n@northeastern.edu>
2024-03-17 18:45:19 -0400

    config log cp

--- startup-scripts/log-config.sh
+++ startup-scripts/log-config.sh
@@ -1,12 +1,16 @@
 pwd
-cd /var/log
+
+# shellcheck disable=SC2164
+cd ../csye6225/webapp/
+sudo cp ./startup-scripts/config.yaml /etc/google-cloud-ops-agent
+pwd
+cd ../../../var/log
+pwd
 sudo mkdir webapp
 chmod u+w /var/log/webapp
 echo 'coppying config'
-pwd
-# shellcheck disable=SC2164
-cd /home/csye6225/webapp
-sudo cp ./startup-scripts/config.yaml /etc/google-cloud-ops-agent
+
+
 
 sudo vi /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
